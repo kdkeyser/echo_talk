66c14c4e134feab7f0a414c6cbd932287d1df185
Mark Christiaens <mark.christiaens@hgst.com>
2015-10-16 17:02:25 +0200

    Some fixes

--- go_server/echo.go
+++ go_server/echo.go
@@ -1,32 +1,40 @@
 package main
 
 import (
+    "runtime"
 	"fmt"
 	"net"
-	"bufio"
-        "bytes"
+    "bytes"
 )
 
 func echo(s net.Conn, i int) {
 	defer s.Close()
+    recv := make([]byte, 65536)
 	fmt.Printf("%d: %v <-> %v\n", i, s.LocalAddr(), s.RemoteAddr())
-	b := bufio.NewReader(s)
-        exit_arr := []byte("exit\r\n")
 	for {
-		line, e := b.ReadBytes('\n')
-		if e != nil {
-			break
-		}
-                if bytes.Equal(line, exit_arr) {
-                        break
-                }
-
-		s.Write(line)
+        read := 0
+        for i := 0; ; { 
+		    n, e := s.Read(recv[i:])
+		    if e != nil {
+		    	goto Error
+		    }
+            i += n
+            if recv[i-1] == '\n' {
+                read = i
+                break
+            }
+        }
+        if bytes.Equal(recv, []byte("exit\n")) {
+            break
+        }
+        s.Write(recv[0:read])
 	}
+Error:
 	fmt.Printf("%d: closed\n", i)
 }
 
 func main() {
+    fmt.Printf("%d\n", runtime.NumCPU())
 	l, e := net.Listen("tcp", ":6000")
 	for i := 0; e == nil; i++ {
 		var s net.Conn
