3a37106d2c490c5e4ec422fb6eb18142c0e9c139
Mark Christiaens <mark.g.j.christiaens@gmail.com>
2015-12-05 16:20:23 +0000

    Differentiate server threads vs. driver threads

--- time.bash
+++ time.bash
@@ -1,21 +1,22 @@
 #!/bin/bash
-set -ex
+set -e
 
-TEST_CONNECTION_NUMBER=10
-TEST_MESSAGE_LENGTH=30
-THREADS=2
-TEST_DURATION=2
+TEST_CONNECTION_NUMBER=1000
+TEST_MESSAGE_LENGTH=1000
+DRIVER_THREADS=2
+SERVER_THREADS=3
+TEST_DURATION=10
 
 run_driver () {
         cd go_driver
-        export GOMAXPROCS=2
+        export GOMAXPROCS=${DRIVER_THREADS}
         ./go_driver -a "127.0.0.1:6000" -c ${TEST_CONNECTION_NUMBER} -l ${TEST_MESSAGE_LENGTH} -t ${TEST_DURATION}
         cd -
 }
 
 test_go () {
         cd go_server
-        export GOMAXPROCS=${THREADS}
+        export GOMAXPROCS=${SERVER_THREADS}
         # ./go_server > /dev/null 2> /dev/null &
         ./go_server &
         GO_SERVER_PID=$!
@@ -29,8 +30,8 @@ test_go () {
 
 test_conduit () {
         cd echo-server-conduit
-        # stack exec Echo -- +RST -N ${THREADS} -RTS > /dev/null 2> /dev/null &
-        stack exec Echo -- +RST -N ${THREADS} -N3 -A16M -kc4k -RTS &
+        # stack exec Echo -- +RST -N ${SERVER_THREADS} -RTS > /dev/null 2> /dev/null &
+        stack exec Echo -- +RST -N ${SERVER_THREADS} -A16M -kc4k -RTS &
         ECHO_SERVER_CONDUIT_PID=$!
         sleep 1
         cd -
@@ -42,8 +43,8 @@ test_conduit () {
 
 test_boost () {
         cd boost_asio_echo/dist/Release/GNU-Linux
-        # ./boost_asio_echo 127.0.0.1 6000 ${THREADS} > /dev/null 2> /dev/null &
-        ./boost_asio_echo 127.0.0.1 6000 ${THREADS} 0 &
+        # ./boost_asio_echo 127.0.0.1 6000 ${SERVER_THREADS} > /dev/null 2> /dev/null &
+        ./boost_asio_echo 127.0.0.1 6000 ${SERVER_THREADS} 0 &
         BOOST_ASIO_ECHO_PID=$!
         sleep 1
         cd -
@@ -55,8 +56,8 @@ test_boost () {
 
 test_coio () {
         cd rust-echo-server/target/release
-        #./coio-tcp-echo-server -t ${THREADS} --bind 127.0.0.1:6000 > /dev/null 2> /dev/null &
-        ./coio-tcp-echo-server -t ${THREADS} --bind 127.0.0.1:6000 &
+        #./coio-tcp-echo-server -t ${SERVER_THREADS} --bind 127.0.0.1:6000 > /dev/null 2> /dev/null &
+        ./coio-tcp-echo-server -t ${SERVER_THREADS} --bind 127.0.0.1:6000 &
         COIO_TCP_ECHO_SERVER_PID=$!
         sleep 1
         cd -
@@ -71,6 +72,10 @@ pkill stack || true
 pkill boost_asio_echo || true
 pkill coio-tcp-echo-server || true
 
+echo BOOST ------------------------------------------------------
+test_boost
+sleep 2
+
 echo GO ------------------------------------------------------
 test_go
 sleep 2
@@ -79,10 +84,6 @@ echo CONDUIT ------------------------------------------------------
 test_conduit
 sleep 2
  
-echo BOOST ------------------------------------------------------
-test_boost
-sleep 2
-
 # echo COIO ------------------------------------------------------
 # test_coio
 # sleep 2
