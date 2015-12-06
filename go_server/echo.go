1cc848f0900336f4cf5a6b0670ece5f1d54ca05d
Mark Christiaens <mark.g.j.christiaens@gmail.com>
2015-12-06 08:45:12 +0000

    Reformat Go code

--- go_server/echo.go
+++ go_server/echo.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-    "runtime"
-	"fmt"
-	"net"
-    "bytes"
+        //"runtime"
+        //"fmt"
+        "net"
+        "bytes"
 )
 
 func echo(s net.Conn, i int) {
-	defer s.Close()
-    recv := make([]byte, 65536)
-	// fmt.Printf("%d: %v <-> %v\n", i, s.LocalAddr(), s.RemoteAddr())
-	for {
-        read := 0
-        for i := 0; ; { 
-		    n, e := s.Read(recv[i:])
-		    if e != nil {
-		    	goto Error
-		    }
-            i += n
-            if recv[i-1] == '\n' {
-                read = i
-                break
-            }
+        defer s.Close()
+        recv := make([]byte, 65536)
+        // fmt.Printf("%d: %v <-> %v\n", i, s.LocalAddr(), s.RemoteAddr())
+        for {
+                read := 0
+                for i := 0; ; { 
+                        n, e := s.Read(recv[i:])
+                        if e != nil {
+                                goto Error
+                        }
+                        i += n
+                        if recv[i-1] == '\n' {
+                                read = i
+                                break
+                        }
+                }
+                if bytes.Equal(recv, []byte("exit\n")) {
+                        break
+                }
+                s.Write(recv[0:read])
         }
-        if bytes.Equal(recv, []byte("exit\n")) {
-            break
-        }
-        s.Write(recv[0:read])
-	}
-Error:
-	// fmt.Printf("%d: closed\n", i)
+        Error:
+        // fmt.Printf("%d: closed\n", i)
 }
 
 func main() {
-    fmt.Printf("%d\n", runtime.NumCPU())
-	l, e := net.Listen("tcp", ":6000")
-	for i := 0; e == nil; i++ {
-		var s net.Conn
-		s, e = l.Accept()
-		go echo(s, i)
-	}
+        // fmt.Printf("%d\n", runtime.NumCPU())
+        l, e := net.Listen("tcp", ":6000")
+        for i := 0; e == nil; i++ {
+                var s net.Conn
+                s, e = l.Accept()
+                go echo(s, i)
+        }
 }
