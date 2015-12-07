5f966e44a8ba8874a6b5ed586ae413bd4e95beef
Mark Christiaens <mark.g.j.christiaens@gmail.com>
2015-12-07 20:49:16 +0000

    Use FSM to match exit
    More efficient than matching per line

--- go_server/echo.go
+++ go_server/echo.go
@@ -2,32 +2,92 @@ package main
 
 import (
         //"runtime"
-        //"fmt"
+        "fmt"
         "net"
-        "bytes"
 )
+
+type ExitState int
+
+const (
+        OFFTRACK ExitState = iota
+        NL
+        E
+        X
+        I
+        T
+        CR
+        MATCH
+)
+
+func (es ExitState) to_string () string {
+        switch es {
+                case OFFTRACK : { return "OFFTRACK" }
+                case NL : { return "NL" }
+                case E : { return "E" }
+                case X : { return "X" }
+                case I : { return "I" }
+                case T : { return "T" }
+                case CR : { return "CR" }
+                case MATCH : { return "MATCH" }
+        }
+
+        return ""
+}
+
+type ExitMatcher struct {
+        state ExitState
+}
+
+func newExitMatcher () *ExitMatcher {
+        em := new (ExitMatcher)
+        em.state = NL
+        return em
+}
+
+func (em *ExitMatcher) trans (c byte) {
+        from_state := em.state
+
+        switch em.state {
+                case NL: { if c == 101 { em.state = E } else { em.state = OFFTRACK } }
+                case E: { if c == 120 { em.state = X } else { em.state = OFFTRACK } }
+                case X: { if c == 105 { em.state = I } else { em.state = OFFTRACK } }
+                case I: { if c == 116 { em.state = T } else { em.state = OFFTRACK } }
+                case T: { if c == 13 { em.state = CR } else { em.state = OFFTRACK } }
+                case CR: { if c == 10 { em.state = MATCH } else { em.state = OFFTRACK } }
+                case MATCH: { return }
+                default: { if c == 10 { em.state = NL } else { em.state = OFFTRACK } }
+        }
+
+        to_state := em.state
+
+        fmt.Printf("%d: %s -> %s\n", c, from_state.to_string (), to_state.to_string ());
+
+        return
+}
+
+func (em *ExitMatcher) try_match (cs []byte) bool {
+        for _, c := range cs {
+                em.trans (c)
+        }
+
+        return (em.state == MATCH)
+}
 
 func echo(s net.Conn, i int) {
         defer s.Close()
+        em := newExitMatcher ()
         recv := make([]byte, 65536)
         // fmt.Printf("%d: %v <-> %v\n", i, s.LocalAddr(), s.RemoteAddr())
         for {
-                read := 0
-                for i := 0; ; { 
-                        n, e := s.Read(recv[i:])
-                        if e != nil {
-                                goto Error
-                        }
-                        i += n
-                        if recv[i-1] == '\n' {
-                                read = i
-                                break
-                        }
+                n, e := s.Read(recv[:])
+                if e != nil {
+                        goto Error
                 }
-                if bytes.Equal(recv, []byte("exit\n")) {
+                matched := em.try_match (recv[0:n])
+                if matched {
                         break
-                }
-                s.Write(recv[0:read])
+                } 
+                s.Write(recv[0:n])
         }
         Error:
         // fmt.Printf("%d: closed\n", i)
