007a197f63a32d12466070ba6153a4627e75a079
Mark Christiaens <mark.g.j.christiaens@gmail.com>
2015-12-13 16:09:02 +0000

    Build OCaml, Rust ...

--- build.bash
+++ build.bash
@@ -21,5 +21,17 @@ cd boost_asio_echo
 make all -j4
 cd -
 
+cd echo-server-haskell
+stack build -j4
+cd -
+
+cd ocaml_server
+~/.opam/4.02.3/bin/ocamlfind ocamlopt -w A -syntax camlp4o -linkpkg -package lwt,lwt.syntax,lwt.unix server.ml
+cd -
+
+cd rust-echo-server
+cargo build --release
+cd -
+
 
 
